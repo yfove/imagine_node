8a6730234de6f6a3bd90a8e0e194197e3e8eb111
Harman Singh <h286sing@uwaterloo.ca>
2020-08-19 21:21:08 -0600

    added nodemon

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const app = -express();
+const app = express();
 
 app.listen(3000, () => {
   console.log("server is good");
 });
+
+
+app.get("/", (req, res) => {
+    res.send("this is the home page");
+})
