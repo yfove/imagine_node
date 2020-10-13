97e382151c41e9a565f7b8c37d2044d9b0fc67f9
yfove <curtis.ch.chat@gmail.com>
2020-10-12 19:57:51 -0700

    edit feature now functional

--- routes/blogs.js
+++ routes/blogs.js
@@ -12,9 +12,9 @@ router.get("/new", (req, res) => {
   res.render("blogs/new", { blog: new Blog() })
 });
 
-router.get('/new', async (req, res) => {
-  const blog = await Blog.findByID(req.params.id)
-  res.render('blogs/new', { article: new Blog() })
+router.get('/edit/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id)
+  res.render('blogs/new', { blog: new Blog() })
 })
 
 router.get('/:slug', async (req, res) => {
@@ -42,18 +42,19 @@ router.post('/', async (req, res) => {
 router.post('/', async (req, res, next) => {
   req.blog = new Blog()
   next()
-}, saveArticleAndRedirect('new'))
+}, saveBlogAndRedirect('new'))
 
-router.put('/:id', (req, res) => {
-
-})
+router.put('/:id', async (req, res, next) => {
+  req.blog = await blog.findById(req.params.id)
+  next()
+}, saveBlogAndRedirect('new'))
 
 router.delete('/:id', async (req, res) => {
   await Blog.findByIdAndDelete(req.params.id)
   res.redirect('/blogs')
 })
 
-function saveArticleAndRedirect(path) {
+function saveBlogAndRedirect(path) {
   return async (req, res) => {
     let blog = req.blog
     blog.title = req.body.title,
