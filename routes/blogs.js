7152ab861c1c30a182d9effb43a950938a132d16
yfove <curtis.ch.chat@gmail.com>
2020-10-12 18:23:02 -0700

    function for edit

--- routes/blogs.js
+++ routes/blogs.js
@@ -12,6 +12,11 @@ router.get("/new", (req, res) => {
   res.render("blogs/new", { blog: new Blog() })
 });
 
+router.get('/new', async (req, res) => {
+  const blog = await Blog.findByID(req.params.id)
+  res.render('blogs/new', { article: new Blog() })
+})
+
 router.get('/:slug', async (req, res) => {
   const blog = await Blog.findOne({ slug: req.params.slug })
   // res.send(req.params.id)
@@ -34,9 +39,34 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.post('/', async (req, res, next) => {
+  req.blog = new Blog()
+  next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', (req, res) => {
+
+})
+
 router.delete('/:id', async (req, res) => {
   await Blog.findByIdAndDelete(req.params.id)
   res.redirect('/blogs')
 })
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let blog = req.blog
+    blog.title = req.body.title,
+      blog.description = req.body.description,
+      blog.markdown = req.body.markdown
+    try {
+      blog = await blog.save()
+      res.redirect(`/blogs/${blog.slug}`)
+    } catch (e) {
+      console.log(e)
+      res.render(`blogs/${path}`, { blog: blog })
+    }
+  }
+}
 
 module.exports = router;
